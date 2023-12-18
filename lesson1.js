0d3c70df7c953987ab684d8162cae6df3c3ad13c
Dmitriy-Rassol <devlion69@gmail.com>
2023-12-18 03:02:54 +0300

    создание проекта

---
+++ lesson1.js
@@ -0,0 +1,28 @@
+let homework = "Домашняя работа";
+console.log(homework);
+
+let myName = "Дмитрий";
+let age = 31;
+let familyStatus = false;
+
+console.log(`Имя: ${myName}`);
+console.log(`Возраст: ${age}`);
+console.log(`Есть супруга: ${familyStatus}`);
+
+
+// примеры typeof
+
+// Expected output: "number"
+console.log(typeof 42);
+
+// Expected output: "string"
+console.log(typeof 'blubber');
+
+// Expected output: "boolean"
+console.log(typeof true);
+
+// Expected output: "undefined"ъ
+console.log(typeof undeclaredVariable);
+
+
+
